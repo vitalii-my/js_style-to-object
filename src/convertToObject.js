be8d0510e334b163c827c6f02fa5077bf2136f41
vitalii <vitalii.myhailyk.w@gmail.com>
2025-03-04 23:25:48 +0200

    new version of solution

--- src/convertToObject.js
+++ src/convertToObject.js
@@ -8,14 +8,22 @@
 function convertToObject(sourceString) {
   const result = {};
 
-  sourceString.split(';').forEach(cssDeclaration => {
-    const part = cssDeclaration.split(':');
+  sourceString.split(';').forEach((cssDeclaration) => {
+    const index = cssDeclaration.indexOf(':');
+
+    if (index !==-1) {
+      const key = cssDeclaration.slice(0, index).trim();
+      const value = cssDeclaration.slice(index + 1).trim();
+      result[key] = value;
+    }
+    /* const part = cssDeclaration.split(':');
 
     if (part.length >= 2) {
       const key = part[0].trim();
       const value = part.slice(1).join(':').trim();
+
       result[key] = value;
-    }
+    } */
   });
 
   return result;
